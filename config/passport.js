d22cebdaeb30e23ed1aeee666064c7cf5bf0d368
Santosh Kumar <santosh822kr@gmail.com>
2021-04-26 13:45:39 +0530

    modified callback url

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
+        callbackURL: 'http://studyingroup.herokuapp.com/auth/google/callback'
     }, async (accessToken, refreshToken, profile, done) => {
         const newUser = {
             googleId: profile.id,
